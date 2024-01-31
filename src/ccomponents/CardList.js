44634e9fd0870c80bf3c61c4925d2f917dc99197
Faizan <admin@faizan.com>
2024-01-31 05:32:21 +0500

    using confirm method

--- src/ccomponents/CardList.js
+++ src/ccomponents/CardList.js
@@ -1,42 +1,37 @@
 // CardList.js
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import { data } from "./Data";
 import Navbar from "./Navbar";
 import Main from "./Main";
 import CardDetail from "./CardDetail";
 
 const CardList = () => {
   const [data, setData] = useState([]);
-  
-console.log(process.env.REACT_APP_API_Host,"env variale") //gettttttt   env vraiableeeeeeeee 
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data, "mydata");
-        setData(data.slice(0, 5));
+        setData(data.slice(0, 10));
       });
   }, []);
-  // console.log(data)
 
-  // const deleted = (index) => {
-  //   console.log(index);
-  //   setUserdata(userdata.splice(index, 1));
-  // };
-  const deleted = (index) => {
-    setData((prevData) => prevData.filter((_, i) => i !== index));
+  const handleDelete = (index) => {
+    const isConfirmed = window.confirm("Are you sure you want to delete this card?");
+    console.log(isConfirmed)
+    if (isConfirmed) {
+      setData((prevData) => prevData.filter((_, i) => i !== index));
+    }
   };
+
   return (
     <div>
-      
       <Navbar />
       <Main />
       {data.map((item, index) => (
-        <div className="container-fluid  ">
-          <Link to={`/card/${item.id}`} state={{data:item}}>
-            <div 
+        <div key={index} className="container-fluid">
+          <Link to={`/card/${item.id}`} state={{ data: item }}>
+            <div
               style={{
                 border: "1px solid #ddd",
                 padding: "16px",
@@ -49,16 +44,12 @@ console.log(process.env.REACT_APP_API_Host,"env variale") //gettttttt   env vrai
               <p>{item.title}</p>
             </div>
           </Link>
-          <button onClick={() => deleted(index)}>Delete Card</button>
+          <button onClick={() => handleDelete(index)}>Delete Card</button>
         </div>
       ))}
     </div>
   );
 };
-
-// send data as props
-
-
 
 
 export default CardList;
